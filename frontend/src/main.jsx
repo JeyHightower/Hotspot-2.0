5d915ed545b63e0aec8ee85edd343ed5bcd62994
JeyHightower <Jamesehightower@gmail.com>
2024-11-22 13:16:54 -0600

    phase 0 end

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import configureStore from './store/store';
 import { Provider } from 'react-redux';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 
 const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+  
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
