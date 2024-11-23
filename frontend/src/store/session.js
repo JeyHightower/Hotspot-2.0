41a7362ce84be505fc6028f7052f93934909a5e4
JeyHightower <Jamesehightower@gmail.com>
2024-11-22 18:12:21 -0600

    sessions thunks

---
+++ frontend/src/store/session.js
@@ -0,0 +1,57 @@
+import { csrfFetch } from './csrf';
+
+//!ACTION TYPES:
+const SET_SESSION_USER = 'session/set_session_user';
+const REMOVE_SESSION_USER = 'session/remove_session_user';
+
+//!ACTION CREATORS:
+const setSessionUser = (user) => {
+  return {
+    type: SET_SESSION_USER,
+    payload: user,
+  };
+};
+
+const removeSessionUser = () => {
+  return {
+    type: REMOVE_SESSION_USER,
+  };
+};
+
+//!THUNK ACTIONS:
+export const loginThunk = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setSessionUser(data.user));
+  return response;
+};
+
+//!INITIAL STATE:
+const initialState = {
+  user: null,
+};
+
+//!REDUCERS:
+const sessionReducer = (state = initialState, action) => {
+  const handlers = {
+    [SET_SESSION_USER]: (state, action) => ({
+      ...state,
+      user: action.payload,
+    }),
+    [REMOVE_SESSION_USER]: (state) => ({
+      ...state,
+      user: null,
+    }),
+  };
+  const handler = handlers[action.type];
+  return handler ? handler(state, action) : state;
+};
+
+export default sessionReducer;
