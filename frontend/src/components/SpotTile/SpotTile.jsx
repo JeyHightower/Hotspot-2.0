891325b868b56bb897476eb32daba5b22e900773
JeyHightower <Jamesehightower@gmail.com>
2024-12-05 18:15:06 -0600

    completed proj

--- frontend/src/components/SpotTile/SpotTile.jsx
+++ frontend/src/components/SpotTile/SpotTile.jsx
@@ -1,36 +1,40 @@
-import { useNavigate } from 'react-router-dom';
 import { FaStar } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 import './SpotTile.css';
 
 const SpotTile = ({ spot }) => {
-    const navigate = useNavigate();
-    const defaultImage = 'https://placehold.co/600x400?text=No+Image';
-   
-    
-    const imageUrl = spot.previewImage || spot.SpotImages?.[0]?.url || defaultImage;
+  const navigate = useNavigate();
+  const defaultImage = 'https://placehold.co/600x400?text=No+Image';
 
-    return (
-        <div className="spot-tile" onClick={() => navigate(`/spots/${spot.id}`)}>
-            <img 
-                src={imageUrl}
-                alt={spot.name} 
-                className="spot-thumbnail"
-                onError={(e) => {
-                    console.log('Image load error:', e);
-                    e.target.src = defaultImage;
-                }}
-            />
-            <div className="spot-info">
-                <p className="spot-location">{spot.city}, {spot.state}</p>
-                <div className="rating-price">
-                    <span className="rating">
-                        <FaStar /> {spot.avgRating ? Number(spot.avgRating).toFixed(1) : 'New'}
-                    </span>
-                    <p className="spot-price">${spot.price} night</p>
-                </div>
-            </div>
+  const imageUrl =
+    spot.previewImage || spot.SpotImages?.[0]?.url || defaultImage;
+  console.log(imageUrl);
+
+  return (
+    <div className="spot-tile" onClick={() => navigate(`/spots/${spot.id}`)}>
+      <img
+        src={imageUrl}
+        alt={spot.name}
+        className="spot-thumbnail"
+        onError={(e) => {
+          console.log('Image load error:', e);
+          e.target.src = defaultImage;
+        }}
+      />
+      <div className="spot-info">
+        <p className="spot-location">
+          {spot.city}, {spot.state}
+        </p>
+        <div className="rating-price">
+          <span className="rating">
+            <FaStar />{' '}
+            {spot.avgRating ? Number(spot.avgRating).toFixed(1) : 'New'}
+          </span>
+          <p className="spot-price">${spot.price} night</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SpotTile;
