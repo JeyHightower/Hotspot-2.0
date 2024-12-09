332f0c78a25afecbf3731ff14372e23c70cd1958
JeyHightower <Jamesehightower@gmail.com>
2024-12-09 11:41:52 -0600

    updated init-repo.sh

--- backend/init-repo.sh
+++ backend/init-repo.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+
+
+#!/bin/bash
 set -euo pipefail
 
+# Enable debugging
 set -x
 
-npm i
-dotenv prisma generate
-tsc
-dotenv prisma db push --accept-data-loss
+# Install dependencies
+npm install
+
+# Generate Prisma client
+npx dotenv -- npx prisma generate
+
+# Compile TypeScript files
+npx tsc
+
+# Push the Prisma schema to the database
+npx dotenv -- npx prisma db push --accept-data-loss
+
+# set -euo pipefail
+
+# set -x
+
+# npm i
+# dotenv prisma generate
+# tsc
+# dotenv prisma db push --accept-data-loss
