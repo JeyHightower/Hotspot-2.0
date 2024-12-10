73a4c76dee3d2570e395692c8c37d6bf7654205e
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 10:31:18 -0600

    Fix: Update redux-logger import for production

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
-import logger from 'redux-logger'; // Direct import
 import sessionReducer from './session';
 import spotsReducer from './spots';
 
@@ -13,6 +12,7 @@ let enhancer;
 if (import.meta.env.MODE === 'production') {
     enhancer = applyMiddleware(thunk);
 } else {
+    const logger = (await import('redux-logger')).default;
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
