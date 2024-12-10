8b0dba7bdbe72672fabf33e5713e1ac44dc09ef9
JeyHightower <Jamesehightower@gmail.com>
2024-12-09 20:22:45 -0600

    chaged

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  root: 'frontend',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [
     react(),
     eslint({
       lintOnStart: true,
-      failOnError: mode === "production"
-    })
+      failOnError: mode === 'production',
+    }),
   ],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:5005'
+      '/api': 'http://127.0.0.1:5005',
     },
-  }
+  },
 }));
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
-
+// To automatically open the app in the browser whenever the server starts,
+// uncomment the following lines:
+// server: {
+//   open: true
+// }
+Hotspot - 2.0 / frontend / index.html;
