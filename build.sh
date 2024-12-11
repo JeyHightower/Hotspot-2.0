4bcfc346922408785359c52f63d64fc90c6088b1
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 19:05:05 -0600

    james

--- build.sh
+++ build.sh
@@ -3,24 +3,26 @@
 set -euo pipefail
 set -x
 
+# Setup pnpm
+npm install -g pnpm
+pnpm setup
+source ~/.bashrc
+
 # Backend setup
 cd backend || { echo "Failed to change directory to backend"; exit 1; }
 
-# Install pnpm
-npm install -g pnpm
-
 # Install dependencies using pnpm
 pnpm install
-pnpm add -g typescript
-pnpm add @types/express @types/node typescript --save-dev
+pnpm add typescript --save-dev
+pnpm add @types/express @types/node --save-dev
 pnpm add prisma @prisma/client
 
 # Generate Prisma client and compile TypeScript
-pnpm dlx prisma generate
-npx tsc
+pnpm exec prisma generate
+pnpm exec tsc
 
 # Database updates
-pnpm dlx prisma db push --accept-data-loss
+pnpm exec prisma db push --accept-data-loss
 cd ..
 
 # Frontend setup
