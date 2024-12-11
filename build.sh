e0757e9d4f80ca91cebdb3c295fbfff2411c06ba
JeyHightower <Jamesehightower@gmail.com>
2024-12-11 14:55:49 -0600

    schg

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 set -x
 
 cd backend
-  pnpm i
-  pnpm prisma generate
-  pnpm tsc
-  pnpm prisma db push --accept-data-loss
+pnpm add -g prisma
+pnpm i
+pnpm prisma generate
+pnpm tsc
+pnpm prisma db push --accept-data-loss
 cd ..
-
 cd frontend
   pnpm i
   pnpm run build
