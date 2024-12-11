230b7cce38ef623675c3aa7c149e8af052527575
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 18:42:29 -0600

    added sh script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+# Backend setup
+cd backend || { echo "Failed to change directory to backend"; exit 1; }
+npm install
+npm install typescript --save-dev
+npm install @prisma/client prisma --save-dev
+npx tsc
+npx prisma generate
+npx prisma db push --accept-data-loss
+cd ..
+
+# Frontend setup
+cd frontend || { echo "Failed to change directory to frontend"; exit 1; }
+npm install
+npm install @vitejs/plugin-react --save-dev
+npm run build
+cd ..
