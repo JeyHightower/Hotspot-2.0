f997d83bd5879043e6249e0ab1bd13d4136c4791
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 19:53:07 -0600

    koji

--- build.sh
+++ build.sh
@@ -12,21 +12,21 @@ rm -rf prisma/client
 # Install yarn
 npm install -g yarn
 
-# Install all dependencies including types
+# Install dependencies in correct order
 yarn install
 yarn add typescript @types/express @types/node --dev
 yarn add prisma --dev
-yarn add @prisma/client
+yarn add @prisma/client --force
 yarn add --dev @types/express @types/node ts-node
 
-# Generate Prisma client
-yarn prisma generate
+# Generate Prisma client using npx
+npx prisma generate
 
 # Run TypeScript compilation
 yarn exec tsc
 
 # Database updates
-yarn prisma db push --accept-data-loss
+npx prisma db push --accept-data-loss
 cd ..
 
 # Frontend setup
