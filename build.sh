45ba91433731f6dd9c2be34c129ee73f9c620662
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 19:07:53 -0600

    wow

--- build.sh
+++ build.sh
@@ -3,31 +3,25 @@
 set -euo pipefail
 set -x
 
-# Setup pnpm
-npm install -g pnpm
-pnpm setup
-source ~/.bashrc
-
 # Backend setup
 cd backend || { echo "Failed to change directory to backend"; exit 1; }
 
-# Install dependencies using pnpm
-pnpm install
-pnpm add typescript --save-dev
-pnpm add @types/express @types/node --save-dev
-pnpm add prisma @prisma/client
+# Install dependencies using npm
+npm install
+npm install typescript @types/express @types/node --save-dev
+npm install prisma @prisma/client
 
 # Generate Prisma client and compile TypeScript
-pnpm exec prisma generate
-pnpm exec tsc
+npx prisma generate
+npx tsc
 
 # Database updates
-pnpm exec prisma db push --accept-data-loss
+npx prisma db push --accept-data-loss
 cd ..
 
 # Frontend setup
 cd frontend || { echo "Failed to change directory to frontend"; exit 1; }
-pnpm install
-pnpm add @vitejs/plugin-react --save-dev
-pnpm run build
+npm install
+npm install @vitejs/plugin-react --save-dev
+npm run build
 cd ..
