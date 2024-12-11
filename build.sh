e9ab70d22bbe57a26df4ba1997eb953d707f1bc7
JeyHightower <Jamesehightower@gmail.com>
2024-12-10 20:02:17 -0600

    lhfd

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@ set -x
 
 cd backend
 
-# Clean start
+# Backend setup
 rm -rf node_modules package-lock.json yarn.lock
 rm -rf prisma/client
 
-# Install yarn
 npm install -g yarn
 
-# Install dependencies with all necessary types
 yarn install
 yarn add typescript@5.7.2 @types/express@5.0.0 @types/node@22.10.1 --dev
 yarn add prisma@5.7.0 --dev
@@ -20,19 +18,16 @@ yarn add @prisma/client@5.7.0
 yarn add --dev ts-node@10.9.2
 yarn add --dev @types/express-serve-static-core
 
-# Generate Prisma client
 NODE_ENV=development npx prisma generate
-
-# Run TypeScript compilation
 yarn exec tsc
-
-# Database updates
 npx prisma db push --accept-data-loss
 cd ..
 
-# Frontend setup
+# Frontend setup with redux-logger
 cd frontend
+rm -rf node_modules package-lock.json yarn.lock
 yarn install
 yarn add @vitejs/plugin-react --dev
+yarn add redux-logger @types/redux-logger
 yarn build
 cd ..
