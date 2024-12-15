ba74ae5ce7383f27bda7913305a9ba35abf6af36
JeyHightower <Jamesehightower@gmail.com>
2024-12-14 21:26:24 -0600

    build runs from back to front 0 errors locally

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,6 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-// //@ts-expect-error no typeshed
-// import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    // eslint({
-    //   lintOnStart: true,
-    //   failOnError: mode === 'production',
-    // }),
-  ],
-  server: {
-    proxy: { '/api': 'http://localhost:5005' },
-    open: true,
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+});
