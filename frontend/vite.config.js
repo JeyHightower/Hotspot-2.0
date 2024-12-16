02b1780fc45ec3b268f293995197021a565e8c17
JeyHightower <Jamesehightower@gmail.com>
2024-12-16 10:33:58 -0600

    changed vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  },
   plugins: [react()],
 });
