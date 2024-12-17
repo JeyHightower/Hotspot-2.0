2bd4b8bff0d4d20d6cf87d7e16718a57cdb12c43
JeyHightower <Jamesehightower@gmail.com>
2024-12-17 15:34:27 -0600

    yarn prod working now

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,12 +1,14 @@
+import nodeConfig from "./config.node.json" assert { type: "json" };
+
 const data = {
-	environment: process.env["NODE_ENV"] || "development",
-	port: process.env["PORT"] || 5000,
-	dbFile: process.env["DB_FILE"],
-	jwtConfig: {
-		secret: process.env["HelloWorld"],
-		expiresIn: process.env["JWT_EXPIRES_IN"],
-	},
-	isProduction: false,
+  environment: process.env["NODE_ENV"] || "development",
+  port: process.env["PORT"] || 5000,
+  dbFile: process.env["DB_FILE"],
+  jwtConfig: {
+    secret: process.env["JWT_SECRET"] || nodeConfig.security.jwtSecret,
+    expiresIn: process.env["JWT_EXPIRES_IN"] || "604800",
+  },
+  isProduction: false,
 };
 
 data.isProduction = data.environment === "production";
