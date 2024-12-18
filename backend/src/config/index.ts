24da1a570778d3f158435e0e570523ccb9d89eaa
JeyHightower <Jamesehightower@gmail.com>
2024-12-17 20:35:19 -0600

    build is running again

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,13 +1,15 @@
-const nodeConfig = await import("./config.node.json", {
-  assert: { type: "json" },
-});
+import config from "./config.node.json" assert { type: "json" };
+const { security } = config;
+assert: {
+  type: "json";
+}
 
 const data = {
   environment: process.env["NODE_ENV"] || "development",
   port: process.env["PORT"] || 5000,
   dbFile: process.env["DB_FILE"],
   jwtConfig: {
-    secret: process.env["JWT_SECRET"] || nodeConfig.security.jwtSecret,
+    secret: process.env["JWT_SECRET"] || config.security.jwtSecret,
     expiresIn: process.env["JWT_EXPIRES_IN"] || "604800",
   },
   isProduction: false,
