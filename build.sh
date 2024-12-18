0199aa82f33e55668d65c4e34078bc7359059cef
JeyHightower <Jamesehightower@gmail.com>
2024-12-18 11:04:08 -0600

    removed  node from backend

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 set -euo pipefail
 
-# Set environment
 export NODE_VERSION=20.10.0
 export NODE_ENV=production
-export PATH="/opt/render/project/node_modules/.bin:$PATH"
 
-# Setup Yarn
 corepack enable
 yarn set version 4.5.3
 
@@ -15,14 +12,12 @@ echo "Starting production build..."
 
 # Backend build
 cd backend
-rm -rf node_modules
 yarn install
 yarn prisma generate
 yarn build
 
 # Frontend build
 cd ../frontend
-rm -rf node_modules
 yarn install
 yarn build
 
