2160d7001dec3c8634efa1a12ea45ab8d855d192
JeyHightower <Jamesehightower@gmail.com>
2024-12-17 21:26:13 -0600

    updated index.ts

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,15 +1,20 @@
-import config from "./config.node.json" assert { type: "json" };
-const { security } = config;
-assert: {
-  type: "json";
-}
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nodeConfig = JSON.parse(
+  fs.readFileSync(join(__dirname, "./config.node.json"), "utf8")
+);
 
 const data = {
   environment: process.env["NODE_ENV"] || "development",
   port: process.env["PORT"] || 5000,
   dbFile: process.env["DB_FILE"],
   jwtConfig: {
-    secret: process.env["JWT_SECRET"] || config.security.jwtSecret,
+    secret: process.env["JWT_SECRET"] || nodeConfig.security.jwtSecret,
     expiresIn: process.env["JWT_EXPIRES_IN"] || "604800",
   },
   isProduction: false,
