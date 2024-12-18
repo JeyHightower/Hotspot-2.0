42c502c2b9d81570d8438bf36cd8e326f56a08ad
JeyHightower <Jamesehightower@gmail.com>
2024-12-17 21:31:47 -0600

    changes index.ts file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,20 +1,9 @@
-import fs from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const nodeConfig = JSON.parse(
-  fs.readFileSync(join(__dirname, "./config.node.json"), "utf8")
-);
-
 const data = {
   environment: process.env["NODE_ENV"] || "development",
   port: process.env["PORT"] || 5000,
   dbFile: process.env["DB_FILE"],
   jwtConfig: {
-    secret: process.env["JWT_SECRET"] || nodeConfig.security.jwtSecret,
+    secret: process.env["JWT_SECRET"] || "HelloWorld",
     expiresIn: process.env["JWT_EXPIRES_IN"] || "604800",
   },
   isProduction: false,
