5b18fab9796327f02c86a0c31ed0f0a04759ade7
JeyHightower <Jamesehightower@gmail.com>
2024-12-28 14:26:47 -0600

    fixed errors running locally and devs work

--- build.sh
+++ build.sh
@@ -7,12 +7,23 @@ export NODE_ENV=production
 # Activate npm
 corepack prepare npm@latest --activate
 
-# Install all dependencies
-npm run install:all
+# backend
+cd backend
 
-# Database setup
-npm run db:migrate
-npm run db:seed
+# Install dependencies and type definitions
+npm install
+npm install --save-dev @types/bcryptjs @types/cookie-parser @types/cors @types/csurf \
+  @types/express @types/jsonwebtoken @types/morgan express-validator
 
-# Build both apps
+# Generate Prisma client and run migrations
+npx prisma generate
+npx prisma migrate deploy
+npx prisma db seed
+
+# frontend
+cd ../frontend
+npm install
 npm run build
+cd ..
+
+
