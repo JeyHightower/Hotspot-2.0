575c97b95c8680d2b44ce4ff67f5c4366b566baf
JeyHightower <Jamesehightower@gmail.com>
2025-01-01 09:48:08 -0600

    favicon and logo added

--- frontend/src/components/Header-Logo/Header-Logo.jsx
+++ frontend/src/components/Header-Logo/Header-Logo.jsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom';
-import './Header-Logo.css';
+import { Link } from "react-router-dom";
+import "./Header-Logo.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <Link to='/'>
-                <img 
-                    src="/hotspot-logo.svg"
-                    alt="HotSpotS"
-                    className="header-logo"
-                    width="150"
-                    height="40"
-                />
-            </Link>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <Link to="/">
+        <img
+          src="/hotspot-logo.svg"
+          alt="HotSpotS"
+          className="header-logo"
+          width="150"
+          height="40"
+        />
+      </Link>
+    </header>
+  );
+};
 export default Header;
